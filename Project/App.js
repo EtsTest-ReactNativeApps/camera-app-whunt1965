515aff205f47599e9a556322dc78c40d3b171f76
Wiley Hunt <johnhunt@MacBook-Pro.local>
2021-03-23 18:12:43 -0400

    Ugly app with base components

--- Project/App.js
+++ Project/App.js
@@ -8,28 +8,46 @@
 
 // React Native Counter Example using Hooks!
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet, FlatList, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
+import 'react-native-camera'
 
 
 const App = () => {
   const [count, setCount] = useState(0);
-
-  function getInitialState() {
-    return {
-      region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-    };
-  }
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
   
-  function onRegionChange(region) {
-    this.setState({ region });
-  }
+  const onRegionChange = ({region}) => setRegion(region);
+
+  useEffect(() => {
+    const geo =navigator.geolocation;
+    if(!geo){
+      console.log("Couldn't get geolocation")
+      return;
+    }
+    navigator.geolocation.watchPosition(
+      (position) => {
+          console.log("wokeeey");
+          console.log(position);
+          setRegion({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          })
+      });
+  }, [])
+
+  takePicture = async () => {
+    if (this.camera) {
+      const data = await this.camera.takePictureAsync();
+      console.warn('takePictureResponse ', data);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -40,14 +58,18 @@ const App = () => {
         title="Click me!"
       />
       </View>
+      <View style={styles.cam}>
+      <Text style={styles.text2}>Take a Picture</Text>
+      <Button
+        onPress={() => takePicture()}
+        title="Snap!"
+      />
+      </View>
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        region={region}
+        onRegionChange={onRegionChange}
+        showsUserLocation={true} 
       />
     </View>
 
@@ -65,6 +87,17 @@ const styles = StyleSheet.create({
     flex: 2,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  cam: {
+    flex: 2,
+    color: '#660066',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text2:{
+    color: '#660066',
+    fontWeight: 'bold',
   },
   map: {
     flex: 2,
