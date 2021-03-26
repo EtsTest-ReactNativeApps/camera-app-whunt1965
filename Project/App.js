d2449406fcca3e829e07740b94b7dada97692080
Wiley Hunt <johnhunt@MacBook-Pro.local>
2021-03-26 16:05:18 -0400

    Initial Working nav

--- Project/App.js
+++ Project/App.js
@@ -1,120 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-// React Native Counter Example using Hooks!
-
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, FlatList, Dimensions, SafeAreaView, TouchableHighlight, Image } from 'react-native';
-import MapView from 'react-native-maps';
-import Camera from './components/Camera';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Camera from './components/Camera'
+import Map from './components/Map'
 
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [region, setRegion] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
-  const [img, setImg] = useState(null);
-
-  function onPicture({uri}) {
-    setImg(uri);
-  }
-
-  function onBackToCamera() {
-    setImg(null);
-  }
-  
-  const onRegionChange = ({region}) => setRegion(region);
-
-  useEffect(() => {
-    const geo =navigator.geolocation;
-    if(!geo){
-      console.log("Couldn't get geolocation")
-      return;
-    }
-    navigator.geolocation.watchPosition(
-      (position) => {
-          console.log("wokeeey");
-          console.log(position);
-          setRegion({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          })
-      });
-  }, [])
-
+function HomeScreen({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.text}>
-      <Text>You clicked {count} times</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
       <Button
-        onPress={() => setCount(count + 1)}
-        title="Click me!"
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
       />
-      </View>
-      <View style = {{flex: 2}}>
-      {img ? (
-          <TouchableHighlight
-            style={{flex: 1}}
-            onPress={() => {
-              onBackToCamera();
-            }}>
-            <Image source={{uri: img}} style={{flex: 1}} />
-          </TouchableHighlight>
-        ) : (
-          <Camera onPicture={onPicture} />
-        )}
-      </View>
-      <MapView
-        style={styles.map}
-        region={region}
-        onRegionChange={onRegionChange}
-        showsUserLocation={true} 
+      <Button
+        title="Camera"
+        onPress={() => navigation.navigate('Camera')}
       />
-    </SafeAreaView>
-
+      <Button
+        title="Map"
+        onPress={() => navigation.navigate('Map')}
+      />
+    </View>
   );
-};
+}
 
-// React Native Styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  cam: {
-    flex: 2,
-    color: '#660066',
-    fontWeight: 'bold',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text2:{
-    color: '#660066',
-    fontWeight: 'bold',
-  },
-  map: {
-    flex: 2,
-    // justifyContent: 'center',
-    // alignItems: 'center'
-    // position: relative,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  }
-});
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Camera" component={Camera} />
+        <Stack.Screen name="Map" component={Map} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
